f6857ae0666f92c1529ca37460984b2340db8aeb
moisesfm67 <moisesfm67@hotmail.com>
2023-12-05 17:52:52 -0300

    feat: added docker and kafka

---
+++ cmd/trade/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/moisesfm67/curso-fullcycle/go/internal/infra/kafka"
+	"github.com/moisesfm67/curso-fullcycle/go/internal/market/dto"
+	"github.com/moisesfm67/curso-fullcycle/go/internal/market/entity"
+	"github.com/moisesfm67/curso-fullcycle/go/internal/market/transformer"
+)
+
+func main() {
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
+
+	wg := &sync.WaitGroup{}
+
+	defer wg.Wait()
+
+	kafkaMsgChan := make(chan *ckafka.Message)
+
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": "host.docker.internal:9094",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "latest",
+	}
+
+	producer := kafka.NewKafkaProducer(configMap)
+
+	kafka := kafka.NewConsumer(configMap, []string{"input"})
+
+	go kafka.Consume(kafkaMsgChan)
+
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+
+	go book.Trade()
+
+	go func() {
+		for msg := range kafkaMsgChan {
+
+			wg.Add(1)
+
+			fmt.Println(string(msg.Value))
+
+			tradeInput := dto.TradeInput{}
+
+			err := json.Unmarshal(msg.Value, &tradeInput)
+
+			if err != nil {
+				panic(err)
+			}
+
+			order := transformer.TransformInput(tradeInput)
+
+			ordersIn <- order
+		}
+	}()
+
+	for res := range ordersOut {
+		output := transformer.TransformOutput(res)
+
+		outputJson, err := json.MarshalIndent(output, "", "   ")
+
+		fmt.Println(string(outputJson))
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		producer.Publish(outputJson, []byte("orders"), "output")
+	}
+}
+
+/*
+{
+ "order_id": "1",
+ "investor_id": "Mari",
+ "asset_id": "asset1",
+ "current_shares": 10,
+  "shares": 5,
+  "price": 5.0,
+  "order_type": "SELL"
+}
+
+{
+ "investor_id": "Celia",
+ "asset_id": "asset1",
+ "current_shares": 0,
+  "shares": 5,
+  "price": 5.0,
+  "order_type": "BUY"
+}
+*/
--- go.mod
+++ go.mod
@@ -7,7 +7,10 @@ require (
 	github.com/stretchr/testify v1.8.4
 )
 
+require gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2 // indirect
+
 require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
